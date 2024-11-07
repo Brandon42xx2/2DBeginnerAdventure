e99c34007d9514d4bccbd8cbf261599b6a797dcb
Brandon42xx2 <727529@student.dusd.net>
2024-11-07 11:47:29 -0800

    I added more stuff to my project scirpt.

---
+++ 2DBeginnerAdventureHernandezp4/Assets/UnityTechnologies/Scripts/PlayercontrollerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+        Vector2 postion = rigidbody2d.position;
+        postion.x = postion.x + 5.0f * horizontal * Time.deltaTime;
+        postion.y = postion.y + 5.0f * vertical * Time.deltaTime;
+
+
+        rigidbody2d.MovePosition(postion);
+    }
+}
