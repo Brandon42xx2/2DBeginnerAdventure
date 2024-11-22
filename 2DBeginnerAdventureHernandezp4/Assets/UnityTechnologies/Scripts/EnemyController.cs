f00cbe377e7916a7bed33b392250b0c9849648ef
Brandon42xx2 <727529@student.dusd.net>
2024-11-22 10:25:06 -0800

    Added a scirpt

--- 2DBeginnerAdventureHernandezp4/Assets/UnityTechnologies/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureHernandezp4/Assets/UnityTechnologies/Scripts/EnemyController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,4 +22,27 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+        
+
+        rigidbody2d.MovePosition(position);
+
+    }
+
+
+
+
+
 }
+
+
